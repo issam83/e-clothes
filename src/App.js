6eece5a4e82af5663486f3478493536d4446b842
issam83 <elagrebiissam@gmail.com>
2020-03-27 03:12:33 +0100

    update menu-items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage";
 import "./App.css";
 
 function App() {
+  const Hats = props => {
+    console.log(props);
+    return <div>hats page</div>;
+  };
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={Hats} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -12,32 +12,37 @@ class Directory extends React.Component {
           title: "hats",
           imageUrl:
             "https://cdn.pixabay.com/photo/2015/10/19/08/39/hats-995629_960_720.jpg",
-          id: "1"
+          id: "1",
+          linkUrl: "hats"
         },
         {
           title: "jackets",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: "2"
+          id: "2",
+          linkUrl: ""
         },
         {
           title: "sneakers",
           imageUrl:
             "https://images.pexels.com/photos/2385477/pexels-photo-2385477.jpeg?cs=srgb&dl=pair-of-white-air-jordan-1-s-2385477.jpg&fm=jpg",
-          id: "3"
+          id: "3",
+          linkUrl: ""
         },
         {
           title: "womens",
           imageUrl:
             "https://images.pexels.com/photos/3977543/pexels-photo-3977543.jpeg?cs=srgb&dl=woman-in-black-and-gray-long-sleeve-shirt-3977543.jpg&fm=jpg",
           size: "large",
-          id: "4"
+          id: "4",
+          linkUrl: ""
         },
         {
           title: "mens",
           imageUrl:
             "https://images.pexels.com/photos/2897531/pexels-photo-2897531.jpeg?cs=srgb&dl=man-wearing-black-and-white-paisley-dress-shirt-2897531.jpg&fm=jpg",
           size: "large",
-          id: "5"
+          id: "5",
+          linkUrl: ""
         }
       ]
     };
@@ -46,8 +51,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
